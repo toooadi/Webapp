7d5fe86eb3e418361888bcc86e3721f9dcabfe32
toooadi <toooadi0@gmail.com>
2021-06-07 23:35:55 +0200

    php trials

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@600;700&display=swap" rel="stylesheet">
+    <title>About</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Recipes</a></li>
+            <li><a href="addRecipe.html">Add Recipe</a></li>
+            <li><a href="foodApp.html">Cook</a></li>
+            <li><a href="about.html">About</a></li>
+        </ul>
+    </nav>
+
+<form>
+    <input type="text" name = "num1" placeholder="Number 1">
+    <input type="text" name = "num2" placeholder="Number 2">
+    
+    <select name="operation">
+        <option>None</option>
+        <option>Add</option>
+        <option>Subtract</option>
+        <option>Multiply</option>
+        <option>Divide</option>
+    </select>
+    <br>
+    <button type="submit" name="submit" value="submit"></button>
+</form>
+    
+</body>
+</html>
